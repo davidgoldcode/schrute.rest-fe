98272e7820a78c56894844ddb1035a0187ba6f43
David Gold <davidgold@hey.com>
2021-01-15 20:14:32 -0500

    Updated gitignore, added .env, added basic layout to page

--- dwight/src/App.js
+++ dwight/src/App.js
@@ -1,21 +1,29 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={null} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        A free REST API to generate the best quotes that Scranton has ever seen
       </header>
-    </div>
+      <main>
+        <div>
+          <div>
+            <h1>API</h1>
+            <h3>
+              GET <a href=""></a>
+            </h3>
+            <h3>Tweet</h3>
+            <h3>Refresh</h3>
+          </div>
+          <h3>ttextetesljalsdjfl;kasd</h3>
+        </div>
+      </main>
+      <section>
+        <h1>FAQ</h1>
+      </section>
+    </>
   );
 }
 
