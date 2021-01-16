c599eeaf144a3b9b6ec424d7ad4e7916492c1dd3
David Gold <davidgold@hey.com>
2021-01-15 20:37:42 -0500

    Basic styling layer

--- dwight/src/App.js
+++ dwight/src/App.js
@@ -1,27 +1,58 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
+const faq = [
+  {
+    q: "Can I use this in my project?",
+    a: "Yes! Do whatever you'd like with this",
+  },
+  {
+    q: "Can I use this in my project?",
+    a: "Yes! Do whatever you'd like with this",
+  },
+  {
+    q: "Can I use this in my project?",
+    a: "Yes! Do whatever you'd like with this",
+  },
+];
+
 function App() {
+  const [quote, setQuote] = useState("");
+
+  useEffect(() => {
+    axios.get("");
+  }, []);
+
   return (
     <>
       <header>
-        A free REST API to generate the best quotes that Scranton has ever seen
+        <h1>Schrute.rest</h1>
+        <h3>
+          A free REST API to generate the best quotes that Scranton has ever
+          seen
+        </h3>
       </header>
       <main>
         <div>
           <div>
-            <h1>API</h1>
-            <h3>
+            <h3>API</h3>
+            <span>
               GET <a href=""></a>
-            </h3>
-            <h3>Tweet</h3>
-            <h3>Refresh</h3>
+            </span>
+            <span>Tweet</span>
+            <span>Refresh</span>
           </div>
           <h3>ttextetesljalsdjfl;kasd</h3>
         </div>
       </main>
       <section>
-        <h1>FAQ</h1>
+        <h3>FAQ</h3>
+        {faq.map((item) => (
+          <>
+            <h4>{item.q}</h4>
+            <p>{item.a}</p>
+          </>
+        ))}
       </section>
     </>
   );
